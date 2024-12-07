b9692d325f8eaf937ec14c8b3da51c5f7bf7b671
Noud Verstijnen <noud.verstijnen@gmail.com>
2024-12-07 17:25:04 +0100

    add answer Q5B

--- 2024/answersQ5.ts
+++ 2024/answersQ5.ts
@@ -13,34 +13,80 @@ const formatInput = () => {
   return { rules, updates };
 };
 
-const addValidMiddlePageNumbers = (
+const updateIsValid = (rules: [number, number][], update: number[]) => {
+  let updateIsValid = true;
+  outer: for (let i = 1; i < update.length; i++) {
+    const current = update[i];
+    const invalidPrevious = rules
+      .filter((f) => f[0] === current)
+      .map((f) => f[1]);
+
+    for (let ind = 0; ind < i; ind++) {
+      if (invalidPrevious.includes(update[ind])) {
+        updateIsValid = false;
+        break outer;
+      }
+    }
+  }
+  return updateIsValid;
+};
+
+const addMiddlePageNumbers = (
   rules: [number, number][],
   updates: number[][]
 ) => {
   let sumOfValidMiddlePages = 0;
+  let sumOfReOrderedMiddlePages = 0;
+
+  const invalidUpdates: number[][] = [];
 
   for (const update of updates) {
-    let updateIsValid = true;
-    outer: for (let i = 1; i < update.length; i++) {
-      const current = update[i];
-      const invalidPrevious = rules
-        .filter((f) => f[0] === current)
-        .map((f) => f[1]);
-
-      for (let ind = 0; ind < i; ind++) {
-        if (invalidPrevious.includes(update[ind])) {
-          updateIsValid = false;
-          break outer;
-        }
-      }
-    }
-    if (updateIsValid) {
+    if (updateIsValid(rules, update)) {
       sumOfValidMiddlePages += update[(update.length - 1) / 2];
+    } else {
+      invalidUpdates.push(update);
     }
   }
 
-  return sumOfValidMiddlePages;
+  for (const invalidUpdate of invalidUpdates) {
+    let newUpdate: number[] = [...invalidUpdate];
+    let isValid = false;
+
+    while (!isValid) {
+      for (let i = 1; i < newUpdate.length; i++) {
+        const current = newUpdate[i];
+        const invalidPrevious = rules
+          .filter((f) => f[0] === current)
+          .map((f) => f[1]);
+
+        const firstRuleIndex = newUpdate.findIndex((p) =>
+          invalidPrevious.includes(p)
+        );
+
+        if (firstRuleIndex >= 0 && firstRuleIndex < i) {
+          const updateBuild: number[] = [];
+          newUpdate.forEach((curr, ind) => {
+            if (ind === firstRuleIndex) {
+              updateBuild.push(current);
+              updateBuild.push(curr);
+            } else if (ind !== i) {
+              updateBuild.push(curr);
+            }
+          });
+          newUpdate = updateBuild;
+        }
+      }
+      isValid = updateIsValid(rules, newUpdate);
+    }
+
+    sumOfReOrderedMiddlePages += newUpdate[(newUpdate.length - 1) / 2];
+  }
+
+  return { sumOfValidMiddlePages, sumOfReOrderedMiddlePages };
 };
 
 const { rules, updates } = formatInput();
-console.log(`Answer to Q5A: ${addValidMiddlePageNumbers(rules, updates)}`);
+const { sumOfValidMiddlePages, sumOfReOrderedMiddlePages } =
+  addMiddlePageNumbers(rules, updates);
+console.log(`Answer to Q5A: ${sumOfValidMiddlePages}`);
+console.log(`Answer to Q5B: ${sumOfReOrderedMiddlePages}`);
