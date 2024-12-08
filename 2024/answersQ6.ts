7b0e30ada21cae9517babda6b5c5c545d103b5a6
Noud Verstijnen <noud.verstijnen@gmail.com>
2024-12-08 09:09:37 +0100

    check for loops

--- 2024/answersQ6.ts
+++ 2024/answersQ6.ts
@@ -12,8 +12,24 @@ const formatInput = () => {
       break;
     }
   }
+  const linesWithAddedObsticle: string[][] = [];
+  for (let y = 0; y < lines.length; y++) {
+    for (let x = 0; x < lines[y].length; x++) {
+      if (["#", "^"].includes(lines[y][x])) {
+        continue;
+      }
+      linesWithAddedObsticle.push(
+        lines.map((line, index) => {
+          if (index === y) {
+            return line.slice(0, x) + "0" + line.slice(x + 1);
+          }
+          return line;
+        })
+      );
+    }
+  }
 
-  return { lines, initialPosition };
+  return { initialPosition, lines, linesWithAddedObsticle };
 };
 
 const getNextPosition = (
@@ -33,7 +49,11 @@ const getNextPosition = (
 };
 
 type Direction = "up" | "right" | "down" | "left";
-const createPath = (initialPosition: string, lines: string[]) => {
+const createPath = (
+  initialPosition: string,
+  lines: string[],
+  checkForLoops: boolean = false
+) => {
   const visitedPositions: string[] = [];
   const uniqueVisitedPositions: string[] = [];
   let direction: Direction = "up";
@@ -46,14 +66,40 @@ const createPath = (initialPosition: string, lines: string[]) => {
   const xEndIndex = lines[0].length - 1;
   const yEndIndex = lines.length - 1;
   let hasReachedEnd = false;
+  let isLoop = false;
 
   let currPosition = initialPosition.split(";").map(Number) as [number, number];
-  while (!hasReachedEnd) {
+  while (!hasReachedEnd && !isLoop) {
     const positionString = currPosition.join(";");
-    visitedPositions.push(positionString);
-    if (!uniqueVisitedPositions.includes(positionString)) {
+    if (!visitedPositions.includes(positionString)) {
       uniqueVisitedPositions.push(positionString);
+    } else if (checkForLoops) {
+      const duplicateIndexes = visitedPositions.reduce(
+        (indexes: number[], item, index) => {
+          if (item === positionString) {
+            indexes.push(index);
+          }
+          return indexes;
+        },
+        []
+      );
+      if (duplicateIndexes.length >= 2) {
+        const lastIndex = duplicateIndexes[duplicateIndexes.length - 1];
+        const beforeLastIndex = duplicateIndexes[duplicateIndexes.length - 2];
+        const lastPath = visitedPositions
+          .slice(lastIndex + 1, visitedPositions.length)
+          .join(",");
+        const beforeLastPath = visitedPositions
+          .slice(beforeLastIndex + 1, lastIndex)
+          .join(",");
+
+        if (lastPath === beforeLastPath) {
+          isLoop = true;
+          break;
+        }
+      }
     }
+    visitedPositions.push(positionString);
 
     if (
       currPosition[0] === 0 ||
@@ -68,19 +114,39 @@ const createPath = (initialPosition: string, lines: string[]) => {
     let nextPosition = getNextPosition(currPosition, direction);
     let nextTile = lines[nextPosition[1]][nextPosition[0]];
 
-    while (nextTile === "#") {
+    while (nextTile === "#" || nextTile === "0") {
       direction = newDirectionMap[direction];
       nextPosition = getNextPosition(currPosition, direction);
       nextTile = lines[nextPosition[1]][nextPosition[0]];
     }
     currPosition = nextPosition;
   }
-  return { visitedPositions, uniqueVisitedPositions };
+  return { uniqueVisitedPositions, isLoop };
 };
 
-const { lines, initialPosition } = formatInput();
-const { visitedPositions, uniqueVisitedPositions } = createPath(
-  initialPosition,
-  lines
-);
+const countLoops = (
+  initialPosition: string,
+  linesWithAddedObsticle: string[][]
+) => {
+  let count = 0;
+  let number = 1;
+  let loopNr = 1;
+  console.log("LENGTH", linesWithAddedObsticle.length);
+  for (const lines of linesWithAddedObsticle) {
+    // console.log("IN LOOP", loopNr);
+    loopNr++;
+    number += 1;
+    const { isLoop } = createPath(initialPosition, lines, true);
+    if (isLoop) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+const { initialPosition, lines, linesWithAddedObsticle } = formatInput();
+const { uniqueVisitedPositions } = createPath(initialPosition, lines);
 console.log(`Answer to Q6A: ${uniqueVisitedPositions.length}`);
+console.log(
+  `Answer to Q6B: ${countLoops(initialPosition, linesWithAddedObsticle)}`
+);
